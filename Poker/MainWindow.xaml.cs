23d1fcb308ae1887bd73d4c602efa4f28c9864f2
Berg <paul.berg@mk-versuchsanlagen.de>
2025-12-01 15:29:39 +0100

    Initial commit and Header createt

---
+++ Poker/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Poker
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Konstruktor des MainWindows
+        /// Initialisiert Komponten und die Instanzen der Logikklassen.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        #region Header Settings, Info, Minimize, Close
+
+        /// <summary>
+        /// Methode wird aufgerufen um wieder Balance hinzuzufügen
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddBalance_Click(object sender, RoutedEventArgs e) { }
+
+        /// <summary>
+        /// MEthode wird aufgerufen um das Fenster für die Einstellungen zu öffnen
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Settings_Click(object sender, RoutedEventArgs e) { }
+
+        /// <summary>
+        /// Methode wird aufgerufen um das Fenster für die Infos zu öffnen
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Infos_Click(object sender, RoutedEventArgs e) { }
+
+        /// <summary>
+        /// Methode wird aufgerufen um das Fenster zu minimieren
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+        
+        /// <summary>
+        /// Methode wird aufgerufen um das Fenster zu schließen
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            //Frage ob er wirklich schließen will
+            MessageBoxResult yn = MessageBox.Show("Möchtest du das Programm wirklich schließen?", "Anwendung wird beendet...", MessageBoxButton.YesNo);
+            if (yn == MessageBoxResult.Yes)
+            {
+                this.Close();
+            }           
+        }
+
+        #endregion
+    }
+}
